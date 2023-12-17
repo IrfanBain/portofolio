03ec6f2ba2ecc961c1955a1ff8bbc3aa99f630ab
Irfan Bain <143409309+IrfanBain@users.noreply.github.com>
2023-12-17 19:48:51 +0700

    add file new

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('togel')
+const menu = document.getElementById('menu-2')
+
+button.addEventListener('click', function(){
+    menu.style.display = "flex"
+})
+x.addEventListener('click', function(){
+    menu.style.display = "none"
+})
